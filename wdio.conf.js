ee7f99959920384f59f7789045d8016b02edfc20
Rajesh <35864342+hirajesh@users.noreply.github.com>
2022-07-08 18:18:57 +0530

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -56,8 +56,10 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
-        acceptInsecureCerts: true
+       browserName: 'chrome',
+    chromeOptions: {
+      args: ['--disable-gpu', '--no-sandbox','--headless', '--disable-extensions', '--disable-dev-shm-usage']
+    }
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
